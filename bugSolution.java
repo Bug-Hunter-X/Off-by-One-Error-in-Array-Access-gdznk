4fb3ff623fef518dfcbc40cd42f13604ea7b86df
BugHunter-X <askzenai@gmail.com>
2025-01-27 19:48:06 +0100

    Add bugSolution.java file

---
+++ bugSolution.java
@@ -0,0 +1,12 @@
+public class MyClass {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i * 2;
+        }
+        //Alternatively, use enhanced for loop to avoid index errors
+        for(int num : arr){
+            System.out.println(num);
+        }
+    }
+}
